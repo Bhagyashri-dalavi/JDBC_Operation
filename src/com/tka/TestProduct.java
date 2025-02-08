62e09765f6224fe4db16eb2a95bf2c52350ff685
poojancode <poojanimbalkar9697@gmail.com>
2025-02-08 14:46:32 +0530

    bhagya mam I have add insert query in given task please check

--- src/com/tka/TestProduct.java
+++ src/com/tka/TestProduct.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class TestProduct {
+	
+
 	public static void main(String[] args) {
 		//create connection
 		
@@ -13,8 +15,20 @@ public class TestProduct {
 			//get connection
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydbms1", "root", "root");
 			//write query
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM student");
+			
+			PreparedStatement statement = connection.prepareStatement("Insert into student (id, name,course,cgpa) values(?,?,?,?)");
+			statement.setInt(1,101);
+			statement.setString(2, "pooja");
+			statement.setString(3, "BCA");
+			statement.setString(4, "8.8");
+			
 			//execute query
+			int rows = statement.executeUpdate();
+			
+			if(rows>0) {
+				System.out.println("Student record added successfully");
+			}
+			PreparedStatement ps =connection.prepareStatement("SELECT * FROM student");
 			ResultSet rs = ps.executeQuery();
 			
 			while (rs.next()) {
@@ -23,9 +37,11 @@ public class TestProduct {
 				System.out.print(rs.getString("name")+ " | ");
 				System.out.print(rs.getString("course")+ " | ");
 				System.out.print(rs.getString("cgpa")+ " | ");
-				System.out.println();
-				
+				System.out.println();		
 			}
+			
+		
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
